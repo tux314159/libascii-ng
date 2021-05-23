eb8c91fd1f6d4cbdd28b96875bbe0cdf92e5830b
Isaac <tux314159@gmail.com>
2021-05-23 17:29:55 +0800

    Fixed test2

--- tests/test2.c
+++ tests/test2.c
@@ -6,8 +6,7 @@
 #include <drivers/vt100.h>
 
 char x;
-char s[100];
-char s2[256];
+char s[50];
 
 int main(void)
 {
@@ -19,7 +18,7 @@ int main(void)
     x = ll_stdin_getchar();
     sprintf(s, "You pressed the key '%c'.", x);
     rr_scr_puts(s, (struct screen_coord){0, 0});
-    rr_scr_puts("Press any key to exit.", (struct screen_coord){1, 0});
+    rr_scr_puts("Press any key to exit.", (struct screen_coord){0, 1});
     rr_scr_render();
 
     ll_stdin_getchar();
